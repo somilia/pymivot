eb306636b1c785da000a9d4298e6cf3a90699785
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2022-06-23 11:15:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,20 @@ setup(
     url='https://github.com/ivoa/mivot-validator',
     author='Laurent Michel',
     author_email='laurent.michel@astro.unistra.fr',
-    packages=['measure'],
+    packages=find_packages(),
     install_requires=['xmlschema'],
     version='1.0',
     license='MIT',
     description='Validator for model annotations in VOTable',
     long_description=open('README.md').read(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: >=3.6",
+    ],
+
 )
 
