58a55b505ffc8bb277e13f015241a2182cd6bd59
Laurent Michel <laurent.michel@astro.unistra.fr>
2023-02-27 16:30:48 +0100

    move to version 2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author_email='laurent.michel@astro.unistra.fr',
     packages=find_packages(),
     install_requires=['xmlschema', 'etree'],
-    version='1.1',
+    version='2.0',
     license='MIT',
     description='Validator for model annotations in VOTable',
     long_description=open('README.md').read(),
