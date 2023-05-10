6f98e6e56d04313eb506d82ba93e255759a48d4e
Julien ABID <julien.abid@etu.unistra.fr>
2023-05-10 10:19:18 +0200

    creation of two tests script to test the generic and concrete snippet builders with verification of file existence and file cohesion against pre generated mapping.

---
+++ tests/test_snippet_concrete.py
@@ -0,0 +1,80 @@
+'''
+Testing generations of concrete snippets
+
+Created on 9 May 2023
+@author: julien abid
+'''
+import unittest
+import os
+import filecmp
+from time import sleep
+
+from unittest.mock import patch
+from mivot_validator.launchers.concrete_snippet_launcher import check_args
+from mivot_validator.instance_checking.concrete_snippet_builder import ConcreteBuilder
+
+OUTPUT = os.getcwd() + "/../tmp_snippets/"
+CLASS_NAME = check_args("meas:Position", 0)
+FILE_NAME = "meas.Position.res"
+REF_FILE_NAME = "meas.Position.test"
+CLASSES_LIST = ["meas:Symmetrical", "meas:Asymmetrical2D", "coords:LonLatPoint", "coords:CustomRefLocation",
+                "coords:LonLatPoint", "coords:StdRefLocation", "coords:LonLatPoint", "coords:StdRefLocation"]
+
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        if os.path.exists(OUTPUT + FILE_NAME + ".xml"):
+            os.system("rm " + OUTPUT + FILE_NAME + ".xml")
+            os.system("rm -rf " + os.getcwd() + "/tmp_vodml")
+
+    @classmethod
+    def tearDown(cls):
+        filecmp.clear_cache()
+        print(os.getcwd() + "/tmp_vodml")
+        if os.path.exists(OUTPUT + FILE_NAME + ".xml"):
+            os.system("rm " + OUTPUT + FILE_NAME + ".xml")
+            os.system("rm -rf " + os.getcwd() + "/tmp_vodml")
+
+    @classmethod
+    def tearDownClass(cls):
+        for file in os.listdir(OUTPUT):
+            if file != ".gitkeep":
+                os.remove(OUTPUT + file)
+
+    def testFileExist(self):
+        '''
+        Check that files are generated in the given directory with the right name
+        '''
+        # Given
+        snippet = ConcreteBuilder(CLASS_NAME, OUTPUT, FILE_NAME, CLASSES_LIST)
+
+        # When
+        snippet.build()
+        snippet.outputResult()
+
+        # Then
+        self.assertTrue(os.path.exists(OUTPUT + FILE_NAME + ".xml"))
+
+    def testFileCohesion(self):
+        '''
+        Check that file generated in the given directory have the same content as the test data
+        '''
+        # Given
+        snippet = ConcreteBuilder(CLASS_NAME, OUTPUT, FILE_NAME, CLASSES_LIST)
+
+        # When
+        snippet.build()
+        snippet.outputResult()
+
+        # Then
+        if os.path.exists(OUTPUT + FILE_NAME + ".xml"):
+            self.assertTrue(filecmp.cmp
+                            (OUTPUT + FILE_NAME + ".xml",
+                             os.path.realpath(os.getcwd() + "/../../../tests/data/" + REF_FILE_NAME + ".xml"),
+                             shallow=False))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_snippet_generic.py
@@ -0,0 +1,76 @@
+'''
+Testing generations of generic snippets from a VODML-Model file
+
+Created on 9 May 2023
+@author: julien abid
+'''
+import unittest
+import os
+
+from mivot_validator.utils.xml_utils import XmlUtils
+from mivot_validator.instance_checking.generic_snippet_builder import GenericBuilder
+
+OUTPUT = os.getcwd() + "/../tmp_snippets/coords"
+MODEL = os.getcwd() + "/../vodml/Coords-v1.0.vo-dml.xml"
+MODEL_NAME = "coords"
+
+
+def getObjectTypes(model):
+    '''
+    Get the object types of the given model which are not abstract
+    '''
+    res = []
+
+    for ele in model.xpath(f'.//objectType'):
+        if ele.get("abstract") == "true":
+            continue
+        for tags in ele.getchildren():
+            if tags.tag == "vodml-id":
+                res.append(tags.text)
+    return res
+
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        if os.path.exists(OUTPUT):
+            os.system("rm -rf " + OUTPUT)
+
+    @classmethod
+    def tearDown(cls):
+        if os.path.exists(OUTPUT):
+            os.system("rm -rf " + OUTPUT)
+
+    def testFilesExists(self):
+        '''
+        Check that files are generated in the given directory
+        '''
+        # Given
+        snippets = GenericBuilder(MODEL)
+
+        # When
+        snippets.build()
+
+        # Then
+        self.assertTrue(len(os.listdir(OUTPUT)) > 0)
+
+    def testFilesCohesion(self):
+        '''
+        Check that files generated in the given directory
+        are the object types of the model
+        '''
+        # Given
+        snippets = GenericBuilder(MODEL)
+        object_types = getObjectTypes(XmlUtils.xmltree_from_file(MODEL))
+
+        # When
+        snippets.build()
+
+        # Then
+        for obj in object_types:
+            self.assertTrue(os.path.exists(OUTPUT + '/' + MODEL_NAME + '.' + obj + ".xml"))
+
+
+if __name__ == '__main__':
+    unittest.main()
