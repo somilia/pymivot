c0019e4f1cac3ef705e6273bdd5d38551c2de0ce
Somia Floret <57394764+somilia@users.noreply.github.com>
2023-12-18 13:55:44 +0100

    Add exceptions.py

---
+++ pymivot/tap/exceptions/exceptions.py
@@ -0,0 +1,12 @@
+
+class TableAlreadyExistsException(Exception):
+    """
+    Exception raised when a mapped_table already exists in the database.
+    """
+    pass
+
+class TableDoesNotExistException(Exception):
+    """
+    Exception raised when a mapped_table does not exist in the database.
+    """
+    pass
